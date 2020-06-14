0c0776d0be570dc71c22314c8f3179eded49308b
ziyagurel <ziyagurel55@hotmail.com>
2020-06-14 11:38:20 +0300

    Updates

---
+++ precache-manifest.37cfccd60a423b4592af507f5c081085.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2cb50272119d0e5434ac499f436e129",
+    "url": "/react-deploy-page/index.html"
+  },
+  {
+    "revision": "6cf4cddd61f887ef0514",
+    "url": "/react-deploy-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fc828520b8de7cbac63b",
+    "url": "/react-deploy-page/static/js/2.659d5986.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-page/static/js/2.659d5986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf4cddd61f887ef0514",
+    "url": "/react-deploy-page/static/js/main.ebece262.chunk.js"
+  },
+  {
+    "revision": "ca89361ecc102e9c6e73",
+    "url": "/react-deploy-page/static/js/runtime-main.cb9adcc8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
